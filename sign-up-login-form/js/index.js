8f45623b6f408e0da3e72be50ea3dd1901527bb8
Le Minh Son <son.le@rainmaker-labs.com>
2017-05-19 17:55:27 +0700

    Add sign-up

--- sign-up-login-form/js/index.js
+++ sign-up-login-form/js/index.js
@@ -48,8 +48,8 @@ $(document).ready(function(){
     alert("We've just sent you an email, please check it !");
   });
 
-  //sign-up
-  $("#test").click(function(event){
+  // sign-up
+  $("#submit-register").click(function(event){
     event.preventDefault();
     var response;
     var data = {
@@ -57,23 +57,53 @@ $(document).ready(function(){
       'username': $('.new-username').val(),
       'email': $('.new-email').val(),
       'password': $('.new-password'),
-      'role': 'member'
-    }
+      'role': 'member',
+      'avatar_link': '',
+      'data_of_birth': '1900-01-01',
+      'gender': 'male'
+    };
     $.ajax({
-        url: 'https://floating-woodland-31947.herokuapp.com/api/sign_up',
+        url: 'https://localhost:8042/api/sign_up',
         type: 'POST',
-        data: data,
+        data: JSON.stringify(data),
         dataType : "json",
         contentType: "application/json; charset=utf-8",
         success: function(result) {
-            response = result;
-            console.log(data);
+           alert('success');
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-            response = "err--" + XMLHttpRequest.status + " -- " + XMLHttpRequest.statusText;
-            alert(response);
+            alert('error');
         }
     });
   });
+   // $("#test").click(function(){
+   //        event.preventDefault();
+   //      var response;
+   //      var data = {
+   //        "name": "son",
+   //        "username": "leminhson19965678912345",
+   //        "password": "12347578789012345",
+   //        "email": "abcdsdsd1234d5@gmail.com",
+   //        "avatar_link": "abc.jpg",
+   //        "gender": "male",
+   //        "date_of_birth": "1996-04-02T17:00:00.000Z",
+   //        "role": "member"
+   //      };
+   //      $.ajax({
+   //          url: 'http://localhost:8042/api/sign_up',
+   //          type: 'POST',
+   //          data: JSON.stringify(data),
+   //          dataType : "json",
+   //          contentType: "application/json; charset=utf-8",
+   //          success: function(result) {
+   //              response = result;
+   //              alert('success');
+   //          },
+   //          error: function(XMLHttpRequest, textStatus, errorThrown) {
+   //              response = "err--" + XMLHttpRequest.status + " -- " + XMLHttpRequest.statusText;
+   //              alert(response);
+   //          }
+   //      });
+   //  });
 });
 
